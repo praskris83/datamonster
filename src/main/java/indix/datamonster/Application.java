452e0da002c622f71005ae8e1711a8c270d0c16e
Prasad Krishnan <prasad@Applieddatafinance.com>
2016-08-21 03:15:27 +0530

    Register Watchers using Monitor DSL

--- src/main/java/indix/datamonster/Application.java
+++ src/main/java/indix/datamonster/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import indix.datamonster.handlers.DataProcessor;
-
 /**
  * @author prasad
  *
