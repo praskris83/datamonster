c6ad3b0f0c4dd8d0d110789fed44158c858da339
Prasad Krishnan <prasad@Applieddatafinance.com>
2016-08-20 16:39:57 +0530

    Initial Setup

---
+++ src/main/java/indix/datamonster/Application.java
@@ -0,0 +1,20 @@
+package indix.datamonster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import indix.datamonster.handlers.DataProcessor;
+
+/**
+ * @author prasad
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		DataProcessor.process();
+	}
+}
---
+++ src/main/java/indix/datamonster/handlers/DataProcessor.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package indix.datamonster.handlers;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author prasad
+ *
+ */
+@Service
+public class DataProcessor {
+
+	public static void process(){
+		while (true) {
+			System.out.println("Processing");
+		}
+	}
+}
